e25563f868309909c99697c30c1c63345fafb51d
ysfdgn44 <ysfdgn4445@gmail.com>
2023-01-18 16:44:43 +0300

    sonuc

--- project02/app.js
+++ project02/app.js
@@ -16,53 +16,73 @@ const check8 = document.getElementById("check8");
 const line8 = document.querySelector(".checkbox8");
 const check9 = document.getElementById("check9");
 const line9 = document.querySelector(".checkbox9");
-
+let sayac = 0;
 check1.addEventListener("click", () => {
   check1.classList.toggle("checked");
   line1.classList.toggle("text");
+  if (line1.classList.contains("text")) {
+    sayac++;
+  }
 });
 check2.addEventListener("click", () => {
   check2.classList.toggle("checked");
   line2.classList.toggle("text");
+  if (line2.classList.contains("text")) {
+    sayac++;
+  }
 });
 check3.addEventListener("click", () => {
   check3.classList.toggle("checked");
   line3.classList.toggle("text");
+  if (line3.classList.contains("text")) {
+    sayac++;
+  }
 });
 check4.addEventListener("click", () => {
   check4.classList.toggle("checked");
   line4.classList.toggle("text");
+  if (line4.classList.contains("text")) {
+    sayac++;
+  }
 });
 check5.addEventListener("click", () => {
   check5.classList.toggle("checked");
   line5.classList.toggle("text");
+  if (line5.classList.contains("text")) {
+    sayac++;
+  }
 });
 check6.addEventListener("click", () => {
   check6.classList.toggle("checked");
   line6.classList.toggle("text");
+  if (line6.classList.contains("text")) {
+    sayac++;
+  }
 });
 check7.addEventListener("click", () => {
   check7.classList.toggle("checked");
   line7.classList.toggle("text");
+  if (line7.classList.contains("text")) {
+    sayac++;
+  }
 });
 check8.addEventListener("click", () => {
   check8.classList.toggle("checked");
   line8.classList.toggle("text");
+  if (line8.classList.contains("text")) {
+    sayac++;
+  }
 });
 check9.addEventListener("click", () => {
   check9.classList.toggle("checked");
   line9.classList.toggle("text");
+  if (line9.classList.contains("text")) {
+    sayac++;
+  }
 });
 console.log(line1.classList.contains("text"));
-checked = [
-  "line1",
-  "line2",
-  "line3",
-  "line4",
-  "line5",
-  "line6",
-  "line7",
-  "line8",
-  "line9",
-];
+checked = [line1, line2, line3, line4, line5, line6, line7, line8, line9];
 console.log(checked[0]);
+
+const sonuc = document.querySelector(".sonuc");
+sonuc.innerHTML = `<p>${sayac} is done of 9 task</p>`;
